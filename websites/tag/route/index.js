e3341bd605f31d9c330056033bee89d70cef90d3
vence <vence2013@163.com>
2020-09-16 16:33:17 +0800

    精简标签功能

--- websites/tag/route/index.js
+++ websites/tag/route/index.js
@@ -8,34 +8,6 @@ router.get('/', async (ctx) => {
     await ctx.render('websites/tag/view/index.html'); 
 })
 
-
-router.post('/', async (ctx) => {
-    const TagCtrl = ctx.controls['tag/tag'];
-
-    /* 提取有效的参数 */
-    var req = ctx.request.body;
-    var name = req.name;
-
-    var ret = await TagCtrl.create(ctx, name);
-    if (ret)
-        ctx.body = {'error': 0, 'message': 'success'};
-    else
-        ctx.body = {'error': -1, 'message': '该标签已经存在！'};
-});
-
-
-router.delete('/:tagname', async (ctx) => {
-    const TagCtrl = ctx.controls['tag/tag'];
-
-    /* 提取有效的参数 */
-    var req2 = ctx.params;
-    var tagname = req2.tagname;
-
-    await TagCtrl.delete(ctx, tagname);
-    ctx.body = {'error':  0, 'message': 'success'}
-})
-
-
 /* 搜索标签，参数为：{str, page, size } */
 router.get('/search', async (ctx) => {
     const TagCtrl = ctx.controls['tag/tag'];
@@ -49,7 +21,6 @@ router.get('/search', async (ctx) => {
     var ret = await TagCtrl.search(ctx, str, page, size);
     ctx.body = {'error': 0, 'message': ret};
 })
-
 
 router.get('/check/:str', async (ctx) => {
     const TagCtrl = ctx.controls['tag/tag'];
--- websites/tag/view/js/index.js
+++ websites/tag/view/js/index.js
@@ -4,17 +4,13 @@ loadResource(app).controller('indexCtrl', indexCtrl);
 
 function indexCtrl($scope, $http) 
 {
-    $scope.opts = opts = {'search':'', 'page':1, 'pageSize':100};
-    $scope.page = pageSet(0, opts.pageSize, 10, 0);
+    $scope.opts = opts = {'str':'', 'page':1, 'size':100};
+    $scope.page = pageSet(0, opts.size, 10, 0);
     $scope.taglist = [];
     $scope.tagname = '';
     $scope.tagsel  = false;
 
     $('.tag-view').height($(document).height() - 100);
-    $('.tag-edit').bind({
-        'mouseenter': ()=>{ $('.tag-edit').css({'opacity':1.0}); },
-        'mouseleave': ()=>{ $('.tag-edit').css({'opacity':0.5}); }
-    });
 
     $scope.$watch('opts', tag_refresh, true);
 
@@ -22,49 +18,12 @@ function indexCtrl($scope, $http)
     {
         $http.get('/tag/search', {params: $scope.opts})
         .then((res)=>{
-            if (errorCheck(res)) 
-                return ;
+            if (errorCheck(res)) return ;
 
             var ret = res.data.message;
             $scope.taglist = ret.list;
-            $scope.page = pageSet(ret.total, opts.pageSize, 10, ret.page);
+            $scope.page = pageSet(ret.total, opts.size, 10, ret.page);
         })        
-    }
-
-    $scope.add = () => {
-        var name = $scope.tagname.replace(/^\s+|\s+$/g,'');
-        if (!name) { return toastr.warning('请输入有效标签！'); }
-
-        $http.post('/tag', {'name': name})
-        .then((res)=>{
-            if (errorCheck(res)) return ;  
-
-            tag_refresh();
-            $scope.tagname = '';
-            toastr.success('创建成功！');            
-        })      
-    }
-
-    $scope.delete = () => {
-        var tagsel = $('.tag-view>div>.badge-success').attr('title');
-
-        $http.delete('/tag/'+tagsel).then((res)=>{
-            tag_refresh();
-            toastr.success('删除成功！');   
-        })
-    }
-
-    /* 再次点击已选中的标签，则取消选中 */
-    $scope.select = (name) => {
-        var tagsel = $('.tag-view>div>.badge-success').attr('title');
-
-        $scope.tagsel = false;
-        $('.badge-success').removeClass('badge-success').addClass('badge-secondary');
-        if (!tagsel || (name != tagsel))
-        {
-            $scope.tagsel = true;
-            $("span[title='"+name+"']").removeClass('badge-secondary').addClass('badge-success');
-        }
     }
 
     $scope.pageGoto = '';
